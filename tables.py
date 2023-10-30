0b022740d00b755ec53616fba3e9cd8ec09632dd
DUOLabs333 <dvdugo333@gmail.com>
2023-10-29 20:11:28 -0400

    Add some tables

---
+++ tables.py
@@ -0,0 +1,42 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+# declarative base class
+class BaseTable(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "USERS"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]
+    password_hash: Mapped[str]
+    creation_time: Mapped[int]
+    user_type: Mapped[int]
+    following: Mapped[str]
+    followers: Mapped[str]
+    balance: Mapped[float]
+    tips: Mapped[float]
+    avatar: Mapped[str]
+    liked_posts: Mapped[str]
+    inbox: Mapped[int]
+
+class Post(Base):
+    __tablename__ = "POSTS"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    author: Mapped[int]=mapped_column(index=True)
+    time_posted: Mapped[int]
+    keywords: Mapped[str]
+    views: Mapped[int]
+    likes: Mapped[int]
+    dislikes: Mapped[int]
+    has_picture: Mapped[bool]
+    has_video: Mapped[bool]
+    post_type: Mapped[int]
+    parent_post: Mapped[int]
+
+class JobApplication(Base):
